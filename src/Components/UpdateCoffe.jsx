a7cee96573adf4f058e09c51cc3fe842e141a6e1
TI Sabbir <tisabbir0@gmail.com>
2024-04-26 12:47:04 +0600

    update done

--- src/Components/UpdateCoffe.jsx
+++ src/Components/UpdateCoffe.jsx
@@ -1,13 +1,102 @@
 import { useLoaderData } from "react-router-dom";
+import Swal from "sweetalert2";
 
 
 const UpdateCoffe = () => {
 
     const coffee = useLoaderData();
+    const {
+       _id, name, quantity, supplier, taste, category, details, photo
+    } = coffee;
+
+    const handleUpdate = e =>{
+        e.preventDefault();
+        const form = e.target;
+        const name = form.name.value;
+        const quantity = form.quantity.value;
+        const supplier = form.supplier.value;
+        const taste = form.taste.value;
+        const category = form.category.value;
+        const details = form.details.value;
+        const photo = form.photo.value;
+
+        const updatedCoffee = {
+            name, quantity, supplier, taste, category, details, photo
+        }
+        console.log(updatedCoffee);
+        
+        
+        fetch(`http://localhost:5000/coffee/${_id}`,{
+            method: 'PUT',
+            headers:{
+                'content-type' : "application/json"
+            },
+            body: JSON.stringify(updatedCoffee)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            if(data.modifiedCount>0){
+                
+                Swal.fire({
+                    title: 'Success!',
+                    text: 'Updated the coffee successfully',
+                    icon: 'success',
+                    confirmButtonText: 'Cool'
+                  })
+            }
+        })
+
+    }
 
     return (
         <div>
-            <h1>Name of the Coffee : {coffee.name}</h1>
+             <div>
+        <section className="p-6 bg-gray-800 text-gray-50">
+	<form onSubmit={handleUpdate} className="container flex flex-col mx-auto space-y-12">
+		<fieldset className="grid grid-cols-4 gap-6 p-6 rounded-md shadow-sm bg-gray-900">
+			<div className="space-y-2 col-span-full lg:col-span-1">
+				<p className="font-medium">Update The Coffee : {name}</p>
+				<p className="text-xs">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci fuga autem eum!</p>
+			</div>
+			<div className="grid grid-cols-6 gap-4 col-span-full lg:col-span-3">
+				<div className="col-span-full sm:col-span-3">
+					<label htmlFor="name" className="text-sm">Name</label>
+					<input defaultValue={name} name="name" id="name" type="text" placeholder="Name" className="w-full rounded-md focus:ring focus:ring-opacity-75 text-gray-900 focus:ring-violet-400 border-gray-700" />
+				</div>
+				<div className="col-span-full sm:col-span-3">
+					<label htmlFor="chef" className="text-sm">Quantity</label>
+					<input defaultValue={quantity} name="quantity" type="text" placeholder="quantity" className="w-full rounded-md focus:ring focus:ring-opacity-75 text-gray-900 focus:ring-violet-400 border-gray-700" />
+				</div>
+
+				<div className="col-span-full sm:col-span-3">
+					<label className="text-sm">Supplier</label>
+					<input defaultValue={supplier} name="supplier" id="supplier" type="text" placeholder="Supplier" className="w-full rounded-md focus:ring focus:ring-opacity-75 text-gray-900 focus:ring-violet-400 border-gray-700" />
+				</div>
+
+				<div className="col-span-full">
+					<label htmlFor="address" className="text-sm">Taste</label>
+					<input defaultValue={taste} name="taste" id="address" type="text" placeholder="" className="w-full rounded-md focus:ring focus:ring-opacity-75 text-gray-900 focus:ring-violet-400 border-gray-700" />
+				</div>
+
+				<div className="col-span-full sm:col-span-2">
+					<label htmlFor="city" className="text-sm">Category</label>
+					<input defaultValue={category} name="category" id="city" type="text" placeholder="" className="w-full rounded-md focus:ring focus:ring-opacity-75 text-gray-900 focus:ring-violet-400 border-gray-700" />
+				</div>
+				<div className="col-span-full sm:col-span-2">
+					<label htmlFor="state" className="text-sm">Details</label>
+					<input defaultValue={details} name="details" id="state" type="text" placeholder="" className="w-full rounded-md focus:ring focus:ring-opacity-75 text-gray-900 focus:ring-violet-400 border-gray-700" />
+				</div>
+				<div className="col-span-full sm:col-span-2">
+					<label htmlFor="zip" className="text-sm">Photo</label>
+					<input defaultValue={photo} name="photo" id="zip" type="text" placeholder="" className="w-full rounded-md focus:ring focus:ring-opacity-75 text-gray-900 focus:ring-violet-400 border-gray-700" />
+				</div>
+			</div>
+		</fieldset>  
+        <input type="submit"  value="Update Coffee" className="btn btn-secondary" />
+	</form>
+</section>
+       </div>
         </div>
     );
 };
